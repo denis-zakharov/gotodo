6cd3589b4b284dfd38c7609302ee65cc351cda80
Denis Zakharov <dizaharov@gmail.com>
2021-12-08 21:53:54 +0300

    init db

--- go.mod
+++ go.mod
@@ -2,9 +2,13 @@ module github.com/denis-zakharov/gotodo
 
 go 1.17
 
-require github.com/spf13/cobra v1.2.1
+require (
+	github.com/boltdb/bolt v1.3.1
+	github.com/spf13/cobra v1.2.1
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.0.0-20211205182925-97ca703d548d // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "github.com/denis-zakharov/gotodo/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/denis-zakharov/gotodo/cmd"
+	"github.com/denis-zakharov/gotodo/db"
+)
 
 func main() {
+	h := os.Getenv("HOME")
+	dbDir := filepath.Join(h, ".cache/gotodo")
+	must(os.MkdirAll(dbDir, 0755))
+	must(db.Init(filepath.Join(dbDir, "tasks.db")))
+
 	cmd.Execute()
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
